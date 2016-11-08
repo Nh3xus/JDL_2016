92f7cfaf32540de4ad7185683501378f67fbba10
Nh3xus <nicolas.trithardt@sporatux.org>
2016-11-08 02:27:50 +0100

    Add existing file

---
+++ deploy_applications.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+clear
+sleep 3
+sudo apt-get -y install pdfshuffler chromium-browser chromium-browser-l10n pepperflashplugin-nonfree gnome-games openshot openshot-doc frei0r-plugins frei0r-plugins-doc soundconverter audacity teeworlds zram-config clipit
+sudo ldconfig
+exit 0
